3192b7281e3acc70cf78769d78eb7859c39469f8
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-05-06 13:43:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let total = 100;
-total += 50;
-total += 5;
+let addition = 50;
+addition = addition + 5;
+let subtraction = 40;
+subtraction = subtraction - 1;
+let multiplication = 5;
+multiplication = multiplication * 7;
+let division = 6;
+division = division % 2;
